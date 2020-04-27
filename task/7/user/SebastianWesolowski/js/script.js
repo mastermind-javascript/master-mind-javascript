c82c7a374853e7449066ba01cfd2213b92732f2f
Sebastian Wesołowski <sebastian.wesolowski.sw@gmail.com>
2020-04-27 19:56:52 +0200

    task 7

--- task/7/user/SebastianWesolowski/js/script.js
+++ task/7/user/SebastianWesolowski/js/script.js
@@ -1,43 +1,69 @@
 /*
-* Opanuj JavaScript - Przeprogramowani.pl
-* I. Fundamenty języka JavaScript
-*
-* Ćwiczenie 7 - "Emoji"
-*/
+ * Opanuj JavaScript - Przeprogramowani.pl
+ * I. Fundamenty języka JavaScript
+ *
+ * Ćwiczenie 7 - "Emoji"
+ */
 
 /*
-* Cel zadania
-*------------
-* Zaimplementuj funkcję "useEmoji", która zastąpi wszystkie słowa danego parametru zgodnie 
-* z mapowaniem z obiektu "emojiMappings";
-*
-* Przykład:
-* useEmoji('Takiemu zachowaniu mówię stop i to mocny stop!') // => 'Takiemu zachowaniu mówię 🚫 i to mocny 🚫!'
-* useEmoji('Jadę po nowy samochód :D') // => 'Jadę po nowy 🏎 :D'
-*/
+ * Cel zadania
+ *------------
+ * Zaimplementuj funkcję "useEmoji", która zastąpi wszystkie słowa danego parametru zgodnie
+ * z mapowaniem z obiektu "emojiMappings";
+ *
+ * Przykład:
+ * useEmoji('Takiemu zachowaniu mówię stop i to mocny stop!') // => 'Takiemu zachowaniu mówię 🚫 i to mocny 🚫!'
+ * useEmoji('Jadę po nowy samochód :D') // => 'Jadę po nowy 🏎 :D'
+ */
 
 const emojiMappings = {
-    'stop': '🚫',
-    'gwiazda': '⭐️',
-    'samochód': '🏎',
-    'buduję': '🧱',
-    'budzik': '⏰'
+  stop: "🚫",
+  gwiazda: "⭐️",
+  samochód: "🏎",
+  buduję: "🧱",
+  budzik: "⏰",
 };
 
 function useEmoji(input) {
-    return input;
+  console.log(input.split(" ").map((element) => element));
+
+  let mappingInput = input
+    .split(" ")
+    .map((element) => {
+      for (const property in emojiMappings) {
+        if (element.toLowerCase().indexOf(property) == 0) {
+          return element
+            .toLowerCase()
+            .replace(property, emojiMappings[property]);
+        }
+      }
+      return element;
+    })
+    .join(" ");
+  return mappingInput;
 }
 
 /* Weryfikacja */
 
 function verify(input, goal) {
-    if (input === goal) {
-        console.log('Gratulacje!');
-    } else {
-        console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
-    }
+  if (input === goal) {
+    console.log("Gratulacje!");
+  } else {
+    console.log(`Niestety, oczekiwano - ${goal}, otrzymano - ${input}`);
+  }
 }
 
-verify(useEmoji('Takiemu zachowaniu mówię stop i to mocny stop!'), 'Takiemu zachowaniu mówię 🚫 i to mocny 🚫!');
-verify(useEmoji('Jadę po nowy samochód :D'), 'Jadę po nowy 🏎 :D');
-verify(useEmoji('Właśnie buduję swoje skille w JS'), 'Właśnie 🧱 swoje skille w JS');
+verify(useEmoji("stop i to mocny stop!"), "🚫 i to mocny 🚫!");
+// verify(
+//   useEmoji("Takiemu zachowaniu mówię Stop i to mocny stop!"),
+//   "Takiemu zachowaniu mówię 🚫 i to mocny 🚫!"
+// );
+// verify(
+//   useEmoji("Takiemu zachowaniu mówię stop i to mocny stop!"),
+//   "Takiemu zachowaniu mówię 🚫 i to mocny 🚫!"
+// );
+// verify(useEmoji("Jadę po nowy samochód :D"), "Jadę po nowy 🏎 :D");
+// verify(
+//   useEmoji("Właśnie buduję swoje skille w JS"),
+//   "Właśnie 🧱 swoje skille w JS"
+// );
